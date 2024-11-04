13156b6563c022952e756ca829e03062f09ba1eb
Hugues Bousselet <hugues.bousselet@gmail.com>
2024-11-04 13:06:40 +0100

    Initial Commit

---
+++ Emoji Art HBO/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Emoji Art HBO
+//
+//  Created by Hugues Bousselet on 04/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Emoji Art HBO/Emoji_Art_HBOApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_Art_HBOApp.swift
+//  Emoji Art HBO
+//
+//  Created by Hugues Bousselet on 04/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Emoji_Art_HBOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
